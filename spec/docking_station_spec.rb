35fef4d052fb956022c6fffa58b583dfbddd5678
michballard <michballard@gmail.com>
2014-08-11 18:46:51 +0100

    Docking station updated

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -37,4 +37,12 @@ let(:station) { DockingStation.new(:capacity => 20) }
 		expect(lambda { station.dock(bike)}).to raise_error(RuntimeError)
 	end
 
+	it "should provide the list of available bikes" do
+		working_bike, broken_bike = Bike.new, Bike.new
+		broken_bike.break! # reminder - documentation missing exclamation mark
+		station.dock(working_bike)
+		station.dock(broken_bike)
+		expect(station.available_bikes).to eq([working_bike])
+	end
+
 end
