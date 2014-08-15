ff8b2ad301ce4b127502e172f364a0d5da0af586
michballard <michballard@gmail.com>
2014-08-15 10:31:54 +0100

    Van code refactored

--- lib/van.rb
+++ lib/van.rb
@@ -11,30 +11,38 @@ class Van
 
 	def collect_broken_bikes_from(holder)
 		holder.broken_bikes.each do |broken_bike|
-			dock(broken_bike)
-			holder.release(broken_bike)
+			collect_bike(holder, broken_bike)
 		end
 	end
 
 	def release_broken_bikes_to(holder)
 		broken_bikes.each do |broken_bike|
-			holder.dock(broken_bike)
-			release(broken_bike)
+			release_bike(holder, broken_bike)
 		end
 	end
 
 	def collect_fixed_bikes_from(holder)
 		holder.bikes.each do |fixed_bike|
-			dock(fixed_bike)
-			holder.release(fixed_bike)
+			collect_bike(holder, fixed_bike)
 		end
 	end	
 
 	def release_fixed_bikes_to(holder)
 		bikes.each do |fixed_bike|
-			holder.dock(fixed_bike)
-			release(fixed_bike)
+			release_bike(holder, fixed_bike)
 		end
+	end
+
+private
+
+	def collect_bike(holder, bike)
+		dock(bike)
+		holder.release(bike)
+	end
+
+	def release_bike(holder, bike)
+		holder.dock(bike)
+		release(bike)
 	end
 
 end
